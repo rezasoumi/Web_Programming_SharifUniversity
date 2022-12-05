d4bab4a404c70fde2dfb3de9b51a2736518bdf11
hkalbasi <hamidrezakalbasi@protonmail.com>
2022-12-06 02:20:48 +0330

    migrate old index code to new format

--- src/mainpage/index.ts
+++ src/mainpage/index.ts
@@ -12,3 +12,49 @@ darkModeSwitch.addEventListener("change", (e) => {
     document.getElementsByTagName("html")[0].classList.remove("dark");
   }
 });
+
+type FlightData = {
+  from: string;
+  to: string;
+  date: string;
+};
+
+// const typeInput = document.getElementById("type") as HTMLInputElement;
+// typeInput.value = "return";
+// typeInput.onchange = () => {
+//   document.getElementById("date-end").hidden = typeInput.value !== "return";
+// };
+document.getElementById("login").onclick = () => {
+  window.location.href = "/login.html";
+};
+document.getElementById("show-flights").onclick = () => {
+  const data: FlightData = {
+    from: (document.getElementById("from") as HTMLInputElement).value,
+    to: (document.getElementById("to") as HTMLInputElement).value,
+    date: (document.getElementById("date") as HTMLInputElement).value,
+  };
+  sessionStorage.setItem("search-query", JSON.stringify(data));
+  window.location.href = "/flights.html";
+};
+// document.getElementById("my-trips").onclick = () => {
+//   if (sessionStorage.getItem("username")) {
+//     window.location.href = "/dashboard.html";
+//   } else {
+//     window.location.href = "/login.html";
+//   }
+// };
+const addToList = async () => {
+  const existingFlights: FlightData[] = await (
+    await fetch("/flights.json")
+  ).json();
+  existingFlights.forEach((x) => {
+    const add = (lid: string, txt: string) => {
+      const c = document.createElement("option");
+      c.value = txt;
+      document.getElementById(lid).appendChild(c);
+    };
+    add("from-list", x.from);
+    add("to-list", x.to);
+  });
+};
+addToList();
