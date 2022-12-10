cb83681c24e8662c491828256182c80cec393be7
ali.javanmard14 <ali.javanmard14@gmail.com>
2022-12-11 00:30:55 +0330

    colour-login8

--- src/index.ts
+++ src/index.ts
@@ -82,3 +82,14 @@ const addToList = async () => {
   });
 };
 addToList();
+var className = "inverted";
+var scrollTrigger = 60;
+
+window.onscroll = function() {
+  // We add pageYOffset for compatibility with IE.
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByClassName("fixed")[0].classList.add(className);
+  } else {
+    document.getElementsByClassName("fixed")[0].classList.remove(className);
+  }
+};
