aa041e2e6c63f5997e9b9ff59231eb4e196c4902
Reza Soumi <reza.soumi800@gmail.com>
2022-12-11 00:32:48 +0330

    concat pages

--- src/index.ts
+++ src/index.ts
@@ -41,6 +41,7 @@ type FlightData = {
   from: string;
   to: string;
   date: string;
+  number: string;
 };
 
 // const typeInput = document.getElementById("type") as HTMLInputElement;
@@ -56,6 +57,7 @@ document.getElementById("show-flights").onclick = () => {
     from: (document.getElementById("from") as HTMLInputElement).value,
     to: (document.getElementById("to") as HTMLInputElement).value,
     date: (document.getElementById("date") as HTMLInputElement).value,
+    number: (document.getElementById("passengerCount") as HTMLInputElement).value,
   };
   sessionStorage.setItem("search-query", JSON.stringify(data));
   window.location.href = "/flights.html";
